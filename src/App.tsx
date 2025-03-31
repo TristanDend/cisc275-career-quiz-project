783d4c4542574743298d6631fcb183cc47f88171
jfgordon <jfgordon@udel.edu>
2025-03-30 20:38:21 -0400

    added header and footer components and buttons associated with them. No functionality in buttons yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Footer} from './Assests/footer'
+import {Header} from './Assests/header'
 import { Button, Form } from 'react-bootstrap';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
@@ -26,7 +28,7 @@ function App() {
   }
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -45,13 +47,15 @@ function App() {
           Group Members: Tristan Dendorfer, 
           Group Members: Zhenyuan Wang, 
         </p>
-      </header>
+      </header> */}
+      <Header></Header>
       <Form>
         <Form.Label>API Key:</Form.Label>
         <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
         <br></br>
         <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
       </Form>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Assests/footer.tsx
+++ src/Assests/footer.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-export function Header(){
+export function Footer(){
     return (
         <div>
             <Button>
-                Home
+                About Us
             </Button>
             <Button>
-                Simple Quiz
+                Contact Us
             </Button>
-            <Button>
-                Detailed Quiz
-            </Button>
+            
         </div>
     )
     
