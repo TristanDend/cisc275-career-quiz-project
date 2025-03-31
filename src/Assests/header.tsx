33cc9b1ea9952a18df0a12dc1415da83a59185ec
jfgordon <jfgordon@udel.edu>
2025-03-31 11:17:34 -0400

    added states and disabled labels to buttons

--- src/Assests/header.tsx
+++ src/Assests/header.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import { Button } from 'react-bootstrap';
 import '../CSS/header.css'
 
 export function Header(){
+
+    const[isHome, setHome] = useState<Boolean>(true);
+    const[isBasic, setBasic] = useState<Boolean>(false);
+    const[isDetailed, setDetailed] = useState<Boolean>(false);
+
+    function changeActive(buttonName: string){
+        if(buttonName === "home" && isHome === false){
+            setHome(true);
+            setBasic(false);
+            setDetailed(false);
+        }
+        else if(buttonName === "basic" && isBasic === false){
+            setHome(false);
+            setBasic(true);
+            setDetailed(false);
+        }
+        else if(buttonName === "basic" && isBasic === false){
+            setHome(false);
+            setBasic(false);
+            setDetailed(true);
+        }
+
+    }
+
     return (
         <div className = 'header'>
-            <Button className = 'header-button'>
+            <Button className = 'header-button' onClick={() => {changeActive("home")}} disabled = {isHome}> 
                 Home
             </Button>
-            <Button className = 'header-button'>
+            <Button className = 'header-button' onClick={() => {changeActive("basic")}} disabled = {isBasic}>
                 Simple Quiz
             </Button>
-            <Button className = 'header-button'>
+            <Button className = 'header-button' onClick={() => {changeActive("detailed")}} disabled = {isDetailed}>
                 Detailed Quiz
             </Button>
         </div>
