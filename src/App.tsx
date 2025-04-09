a3a548000f16617a6e5f31ec2c3ee0030395acad
jfgordon <jfgordon@udel.edu>
2025-04-09 11:15:06 -0400

    popup in footer are present but do not show up properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import {Footer} from './Components/footer'
 import {Header} from './Components/header'
@@ -36,26 +35,6 @@ function App() {
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <p>
-          Group Members: 
-          Tristan Dendorfer, 
-          Zhenyuan Wang,
-          Jacob Gordon, 
-          Jia Qi
-        </p>
       <Header setOnHome = {setHome} setOnBasic = {setBasic} setOnDetailed = {setDetailed}></Header>
       <Form className = 'App'>
         <Form.Label>API Key:</Form.Label>
--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -12,11 +12,16 @@ interface headerProps {
   
 
 export function Header({setOnHome, setOnBasic, setOnDetailed}: headerProps) {
+    //setting up states
     const [isHome, setHome] = useState<boolean>(true);
     const [isBasic, setBasic] = useState<boolean>(false);
     const [isDetailed, setDetailed] = useState<boolean>(false);
 
+    //Function changes the active button and sets the state of the other buttons to false
+    //It also sets the state of the quiz to be displayed to true or false depending on which button is clicked
     function changeActive(buttonName: string){
+        //case for if the home button is clicked
+        //if the button clicked is not the active button, set the active button to true and the other buttons to false
         if(buttonName === "home" && isHome === false){
             setOnHome(true);
             setOnBasic(false);
@@ -25,6 +30,8 @@ export function Header({setOnHome, setOnBasic, setOnDetailed}: headerProps) {
             setBasic(false);
             setDetailed(false);
         }
+        //case for if the basic button is clicked
+        //if the button clicked is not the active button, set the active button to true and the other buttons to false
         else if(buttonName === "basic" && isBasic === false){
             setOnHome(false);
             setOnBasic(true);
@@ -33,6 +40,8 @@ export function Header({setOnHome, setOnBasic, setOnDetailed}: headerProps) {
             setBasic(true);
             setDetailed(false);
         }
+        //case for if the detailed button is clicked
+        //if the button clicked is not the active button, set the active button to true and the other buttons to false
         else if(buttonName === "detailed" && isDetailed === false){
             setOnHome(false);
             setOnBasic(false);
@@ -44,15 +53,17 @@ export function Header({setOnHome, setOnBasic, setOnDetailed}: headerProps) {
 
     }
 
+    //What is actually shown on the screen
+    //The buttons are disabled if they are the active button
     return (
         <div className = 'header'>
-            <Button className = 'header-button' onClick={() => {changeActive("home")}} disabled = {isHome}> 
+            <Button id = 'header-button' onClick={() => {changeActive("home")}} disabled = {isHome}> 
                 Home
             </Button>
-            <Button className = 'header-button' onClick={() => {changeActive("basic")}} disabled = {isBasic}>
+            <Button id = 'header-button' onClick={() => {changeActive("basic")}} disabled = {isBasic}>
                 Simple Quiz
             </Button>
-            <Button className = 'header-button' onClick={() => {changeActive("detailed")}} disabled = {isDetailed}>
+            <Button id = 'header-button' onClick={() => {changeActive("detailed")}} disabled = {isDetailed}>
                 Detailed Quiz
             </Button>
         </div>
