21087c4e80a3501487544f4b95cb6d913ff1b117
JiaQi312 <148893867+JiaQi312@users.noreply.github.com>
2025-04-22 22:01:49 -0400

    popup working now

--- src/Components/Result.tsx
+++ src/Components/Result.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Popup from 'reactjs-popup';
+import '../CSS/Result.css';
 
 export function ResultPage(): React.JSX.Element {
+    const [loadResults, setLoadResults] = useState<boolean>(true);
+
+    // creates the loading sequence
+    // const loadingHandler = async () => {
+    //     await Promise.all([
+    //         new Promise(resolveTimer => setTimeout(resolveTimer, 5000)),
+    //     ]);
+    // }
+
+    // useEffect runs when the component is rendered. Sets loadResults to false after 5 seconds
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoadResults(false);
+        }, 5000); // 5 seconds
+
+        return () => {clearTimeout(timer)}; // cleanup
+    }, []);
+
     return (
         <div>
-            <h1>Results</h1>
-
+            <h1 id="resultsPageTitle">Results</h1>
+            
+            <Popup open={loadResults} closeOnDocumentClick={false}>
+                {
+                  <div id="ResultsInitialPopup">
+                    Processing Your Answers...
+                  </div>
+                }
+            </Popup>
         </div>
     )
 }
